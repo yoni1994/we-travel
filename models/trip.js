d4e8ea8cd1f4d367cfb87f86beb3ce47601fa17a
vzarate26 <vzarate26@gmail.com>
2021-08-05 18:31:56 -0400

    fix budgetSchema

--- models/trip.js
+++ models/trip.js
@@ -2,15 +2,19 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const budgetSchema = Schema({
+  amount: Number
+})
+
 const tripSchema = Schema({
   name: {type: String},
   img: {type: String},
   attendees:[{type: mongoose.Schema.Types.ObjectId, ref: "Profile"}],
-  budget: {budgetSchema},
+  budget: budgetSchema,
   country: {type: mongoose.Schema.Types.ObjectId, ref: "Country"},
   date: Date,
   notes: String,
-  checklist: [{type: mongoose.Schema.Types.ObjectId, ref: "Country"}]
+  checklist: [{type: mongoose.Schema.Types.ObjectId, ref: "Checklist"}]
 }, {
   timestamps: true,
 })
