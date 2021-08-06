bbd82a6b97ae49fd0b730ffefb3d8155a32d8b33
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-08-06 15:20:34 -0400

    Write create trip controller func

--- controllers/trips.js
+++ controllers/trips.js
@@ -5,7 +5,12 @@ export {
     create
 }
 
-function create(req, res){
-  console.log('Hello world!')
+async function create(req, res){
+    try {
+        const trip = await Trip.create(req.body);  
+        return res.status(201).json(trip)    
+    } catch (err) {
+        return res.status(500).json({err: err.message})
+    }  
 }
 
