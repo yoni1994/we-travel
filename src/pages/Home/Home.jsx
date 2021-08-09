947db963bacb017a2cd0ecf838be3d41bfb152ac
vzarate26 <vzarate26@gmail.com>
2021-08-09 12:48:13 -0400

    import Trips to home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AddTripForm from '../../components/AddTripForm/AddTripForm'
+import Trip from '../../pages/Trips/Trips'
 
 function Home(props) {
   return (
       <div>
           <p>Hello World! I'm the Home component.</p> 
           <AddTripForm/>
+          <Trip/>
       </div>
   )
 }
