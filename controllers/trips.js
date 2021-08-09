9463e48398312798b563fcdce3c83cb983b7b75f
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-08-09 11:49:19 -0400

    Flesh out index func and make minor code styling edits

--- controllers/trips.js
+++ controllers/trips.js
@@ -7,13 +7,18 @@ export {
 
 async function create(req, res){
     try {
-        const trip = await Trip.create(req.body);  
+        const trip = await Trip.create(req.body)  
         return res.status(201).json(trip)    
-    } catch (err) {
-        return res.status(500).json({err: err.message})
+    } catch (error) {
+        return res.status(500).json({error: error.message})
     }  
 }
 
 async function index(req, res){
-    console.log('INDEX CONTROLLER TEST')
+    try {
+        const trips = await Trip.find({})
+        return res.status(200).json(trips)
+    } catch (error) {
+        return res.status(500).json({error: error.message})
+    }
 }
