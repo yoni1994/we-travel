c7f3f0cf4ffbcac1add04b594515044c1a85fcc9
yoni1994 <yoniklein1994@gmail.com>
2021-08-09 10:46:10 -0500

    stub up activities router

--- routes/activities.js
+++ routes/activities.js
@@ -1,1 +1,16 @@
+import { Router } from 'express'
 import * as activitiesCtrl from '../controllers/activities.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+
+// Public Routes
+
+
+// Protected Routes
+router.use(decodeUserFromToken)
+
+export {
+    router
+}
