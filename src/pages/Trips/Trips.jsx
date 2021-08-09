41efc9cc00ff8088561305e590f3092630bb6b86
vzarate26 <vzarate26@gmail.com>
2021-08-09 14:44:11 -0400

    flesh out Trips.jsx

--- src/pages/Trips/Trips.jsx
+++ src/pages/Trips/Trips.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {history} from 'react-router-dom'
 import styles from './Trips.module.css'
-import {
-  createTrip,
-  getTrips,
-} from '../../services/tripService'
+import * as tripService from '../../services/tripService'
 
 function Trips(props) {
+  const [trips, setTrips] = useState([])
+  const handleAddTrip = async (newTripData) => {
+    const newTrip = await tripService.createTrip(newTripData);
+    setTrips([...trips, newTrip]);
+    history.push('/');
+  }
+  
+  const handleDeleteTrip = id => {
+    tripService.deleteOne(id)
+     .then(
+        setTrips(trips.filter(trip => id !== trip._id))
+     )
+    
+  }
+  
+  const handleUpdateTrip = updatedTripData => {
+    tripService.update(updatedTripData)
+     .then(updatedTrip => {
+      const newTripsArray = trips.map(trip => 
+        (trip._id === updatedTrip._id ? updatedTrip : trip)
+      )
+      setTrips(newTripsArray);
+      history.push('/');
+     })
+  }
+  
+  useEffect(() => {
+    tripService.getAll()
+      .then(allTrips => setTrips(allTrips))
+  }, []);
+  
   return (
       <div className={styles.container}>
           <p>Hello World! I'm the Trips component.</p> 
