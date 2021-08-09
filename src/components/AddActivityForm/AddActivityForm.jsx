09dbf0837a021361dc0c570f5d5045835327a232
yoni1994 <yoniklein1994@gmail.com>
2021-08-09 11:27:28 -0500

    stub up AddActivityForm

---
+++ src/components/AddActivityForm/AddActivityForm.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+
+// Styles
+import styles from './AddActivityForm.module.css'
+
+// Services
+
+function AddTripForm(props) {
+  const [formData, setFormData] = useState({
+   
+  })
+  
+ 
+
+  return (
+      <div>
+       
+      </div>
+  )
+}
+
+export default AddTripForm
