cafbf38b7739a772dcc0f2051899e86fb918cb2e
yoni1994 <yoniklein1994@gmail.com>
2021-08-09 15:10:56 -0500

    add activities delete route

--- routes/activities.js
+++ routes/activities.js
@@ -14,6 +14,8 @@ router.get('/', checkAuth, activitiesCtrl.index)
 router.get('/:id', checkAuth, activitiesCtrl.show)
 router.post('/', checkAuth, activitiesCtrl.create)
 router.put('/:id', checkAuth, activitiesCtrl.update)
+router.delete('/:id', checkAuth, activitiesCtrl.delete);
+
 
 export {
     router
