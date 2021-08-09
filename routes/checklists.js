5eed94234abd1b81ba10df0dd957a40563ddecc3
yoni1994 <yoniklein1994@gmail.com>
2021-08-09 15:23:25 -0500

    stub up checklists route

---
+++ routes/checklists.js
@@ -0,0 +1,17 @@
+import { Router } from 'express'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+
+// Public Routes
+
+
+// Protected Routes
+router.use(decodeUserFromToken)
+
+
+
+export {
+    router
+}
