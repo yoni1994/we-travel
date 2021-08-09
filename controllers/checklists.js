9a6baa6dc9f9918b752c61d2951598191e47da9b
yoni1994 <yoniklein1994@gmail.com>
2021-08-09 16:11:04 -0500

    flesh out checklist controller delete function

--- controllers/checklists.js
+++ controllers/checklists.js
@@ -48,8 +48,11 @@ async function update(req, res) {
 
 async function deleteChecklist(req, res){
     try {
-
+        const checklist = await Checklist.findByIdAndDelete(req.params.id);
+        res.status(204).json(checklist);
     } catch (error) {
-
+        console.error(error);
+        res.status(500).json(error);
     }
+    console.log('test delete')
 }
