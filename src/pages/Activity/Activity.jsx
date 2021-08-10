91d27ef1f0e291c408c7af704655dd5765809b96
yoni1994 <yoniklein1994@gmail.com>
2021-08-10 16:01:59 -0500

    save for merging

--- src/pages/Activity/Activity.jsx
+++ src/pages/Activity/Activity.jsx
@@ -5,28 +5,27 @@ import * as activityService from '../../services/activityService'
 import { useParams } from 'react-router-dom'
 
 function Activity(props) {
-//   const { id } = useParams()
-//   const [activity, setActivity] = useState({})
+  const { id } = useParams()
+  const [activity, setActivity] = useState({})
   
-//   useEffect(() => {
-//     const fetchActivity = async () => {
-//         try {
-//             const activity = await activityService.getActivityById(id)
-//             setActivity(activity)
-//         } catch (error) {
-//             throw error
-//         }
-//     }
-//     fetchActivity()
-//     return () => { setActivity(null) }
-//   }, [id])
+  useEffect(() => {
+    const fetchActivity = async () => {
+        try {
+            const activity = await activityService.getActivityById(id)
+            setActivity(activity)
+        } catch (error) {
+            throw error
+        }
+    }
+    fetchActivity()
+    return () => { setActivity(null) }
+  }, [id])
 
   return (
     <div className={styles.container}>
-        <h1>hello</h1>
-        {/* <h1>{activity.name}</h1>
+        <h1>{activity.name}</h1>
         <h2>{activity.notes}</h2>
-        <h3>{activity.date}</h3> */}
+        <h3>{activity.date}</h3>
     </div>
     
   )
