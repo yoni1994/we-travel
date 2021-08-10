416237009c461d0036334b0f2999128d1e8c54c9
yoni1994 <yoniklein1994@gmail.com>
2021-08-10 14:20:37 -0500

    add activities link to trip show page

--- src/pages/Trip/Trip.jsx
+++ src/pages/Trip/Trip.jsx
@@ -43,6 +43,7 @@ function Trip(props) {
       <h1>{trip.name}</h1>
       <h2>{trip.notes}</h2>
       <h3>{trip.date}</h3>
+      <Link to={'/activities'}>Activities</Link>
       <Link to={'/checklist'}>Checklists</Link>
       <button type="button">Edit Trip</button>
       <EditTripForm
