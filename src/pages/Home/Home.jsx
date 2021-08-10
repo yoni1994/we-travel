c43d56d2d627f68c600ca8771df08daa846e61b9
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-08-10 18:28:56 -0400

    Import new styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import styles from './Home.module.css'
+
 function Home(props) {
   return (
-      <div>
+      <div className={styles.container}>
           <h2>Current Trips</h2>
       </div>
   )
