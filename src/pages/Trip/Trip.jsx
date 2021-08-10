67fa168f1cd1e3bcafd2520782c54c85929c0f41
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-08-10 13:55:42 -0400

    Import and use EditTripForm component

--- src/pages/Trip/Trip.jsx
+++ src/pages/Trip/Trip.jsx
@@ -2,11 +2,20 @@ import React, { useEffect, useState } from 'react';
 import styles from './Trip.module.css'
 import * as tripService from '../../services/tripService'
 import { useParams } from 'react-router-dom'
+import EditTripForm from '../../components/EditTripForm/EditTripForm';
 
 function Trip(props) {
-  console.log(props)
   const { id } = useParams()
   const [trip, setTrip] = useState({})
+
+  const handleUpdateTrip = async (updatedTripData) => {
+    try {
+      const updatedTrip = await tripService.update(updatedTripData)
+      setTrip(updatedTrip);
+    } catch (error){
+        throw error
+    } 
+  }
 
   useEffect(() => {
     const fetchTrip = async () => {
@@ -27,6 +36,9 @@ function Trip(props) {
       <h2>{trip.notes}</h2>
       <h3>{trip.date}</h3>
       <button type="button">Edit Trip</button>
+      <EditTripForm
+        handleUpdateTrip={handleUpdateTrip}
+      />
     </div>
   )
 }
