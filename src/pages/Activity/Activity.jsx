ec5196308eefcfa47484d9370d926a5f65e58d91
yoni1994 <yoniklein1994@gmail.com>
2021-08-10 15:47:50 -0500

    import into and stub up activty page

---
+++ src/pages/Activity/Activity.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
+import styles from './Trip.module.css'
+import * as activityService from '../../services/activityService'
+import { useParams } from 'react-router-dom'
+
+function Activity(props) {
+  
+
+  return (
+    <div className={styles.container}>
+     
+    </div>
+    
+  )
+}
+
+export default Activity
