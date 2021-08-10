289eeca341bbcc47c829f05ec23f53e8bd85094d
vzarate26 <vzarate26@gmail.com>
2021-08-10 16:06:03 -0400

    creat addChecklistForm

---
+++ src/components/AddChecklistForm/AddChecklistForm.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+
+import styles from './AddChecklistForm.module.css'
+
+function AddChecklistForm(props) {
+  const [checklistData, setChecklistData] = useState({
+    owner: '',
+    name: '',
+    items: [''],
+  })
+
+  const handleChange = (e) => {
+    setChecklistData({...checklistData, [e.target.owner]: e.target.value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.handleAddChecklist(checklistData)
+    setChecklistData({
+      owner: '',
+      name: '',
+      items: [''],
+    })
+  }
+
+  return (
+    <div>
+      <h2>Add a Checklist</h2>
+    <form id="add-checklist-form" onSubmit={handleSubmit}>
+      <label htmlFor="checklist-name">Name</label>
+      <input 
+      id="checklist-name"
+      type="text"
+      name="name"
+      value={checklistData.name}
+      onChange={handleChange}
+      autoComplete="off"
+      placeholder="Checklist name"
+      />
+      <label htmlFor="checklist-items">Items</label>
+      <input 
+      id="checklist-items"
+      type="text"
+      name="items"
+      value={checklistData.items}
+      onChange={handleChange}
+      autoComplete="off"
+      placeholder="checklist Items"
+      />
+      <button type="submit">Add Checklist</button>
+    </form>
+    </div>
+  )
+}
+
+export default AddChecklistForm
