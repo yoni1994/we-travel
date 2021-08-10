2279e491f208eafb192cbf89712470fc91743b19
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-08-10 15:55:49 -0400

    Create Inspiration folder, component, and css module

---
+++ src/pages/Inspiration/Inspiration.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './Inspiration.module.css'
+
+function Inspiration(){
+    return (
+        <div>
+
+            <h1>Inspiration</h1>
+
+        </div>
+    )
+}
+
+export default Inspiration
