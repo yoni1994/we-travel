df5c6482bec0b36212d77c1493f432e71e869811
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-08-10 18:36:39 -0400

    Add basic elements to Home and make minor styling edits to Inspiration component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
+// styles
 import styles from './Home.module.css'
+
+// services
+
+// components
+import Inspiration from '../Inspiration/Inspiration'
 
 function Home(props) {
   return (
       <div className={styles.container}>
           <h2>Current Trips</h2>
+          <h2>Upcoming Trips</h2>
+          <h2>Plan a New Trip</h2>
+          <button type="button">Add a Trip</button>
+          <p>Not sure where to go?</p>
+          <h2>Get Inspired</h2>
+          <Inspiration/>
       </div>
   )
 }
