01449e087fd0122d4aa6d99b0cb3d9e1526f5192
vzarate26 <vzarate26@gmail.com>
2021-08-11 17:04:00 -0400

    flesh out Checklist.jsx

--- src/pages/Checklists/Checklists.jsx
+++ src/pages/Checklists/Checklists.jsx
@@ -1,50 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Redirect, useHistory } from 'react-router-dom'
 import styles from './Checklists.module.css'
-
-// Services
-import * as checklistService from '../../services/checklistService'
 
 // Components
 import ChecklistCard from '../../components/ChecklistCard/ChecklistCard'
-import AddChecklistForm from '../../components/AddChecklistForm/AddChecklistForm';
+import AddChecklistForm from '../../components/AddChecklistForm/AddChecklistForm'
 
-function Checklists(props) {
-  const history = useHistory()
-  const [checklists, setChecklists] = useState([])
-  
-  const handleAddChecklist = async (newChecklistData) => {
-    const newChecklist = await checklistService.create(newChecklistData);
-    setChecklists([newChecklist, ...checklists]);
+function Checklists(props){
+  const {checklists, handleAddChecklist, handleDeleteChecklist} = props
+  const [showAddChecklistForm, setShowAddChecklistForm] = useState(false)
+  const handleToggle = () => {
+    setShowAddChecklistForm(!showAddChecklistForm)
   }
-  
-  const handleDeleteChecklist = id => {
-    checklistService.deleteOne(id)
-      .then(
-        setChecklists(checklists.filter(checklist => id !== checklist._id))
-      )
-  }
-  
-  useEffect(() => {
-    checklistService.getAll()
-      .then(allChecklists => setChecklists(allChecklists))
-  }, []);
   
   return (
       <div className={styles.container}>
-        <AddChecklistForm
-          handleAddChecklist={handleAddChecklist}
-        ></AddChecklistForm>
-          <h2>Checklists</h2> 
-            {checklists.map(checklist => {
-              return (
-                <ChecklistCard
-                  key={checklist._id}
-                  checklist={checklist}
-                  handleDeleteChecklist={handleDeleteChecklist}
-                /> 
-              )
-            })}
+        <button 
+          type="button"
+          className={styles.plan}
+          onClick={handleToggle}
+        >Add Checklist</button>
+        {showAddChecklistForm && 
+          <AddChecklistForm
+            handleAddChecklist={handleAddChecklist}
+          ></AddChecklistForm>
+        }
+        {checklists?.map((checklist, i) => {
+          return (
+            <ChecklistCard
+              key={i}
+              checklist={checklist}
+              handleDeleteChecklist={handleDeleteChecklist}
+            /> 
+          )
+        })}
       </div>
   )
 }
