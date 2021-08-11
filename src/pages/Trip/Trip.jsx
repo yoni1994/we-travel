703bcaea7e8b296367463034e44be405ec6aced1
vzarate26 <vzarate26@gmail.com>
2021-08-11 18:59:01 -0400

    remove checklist link in Trip

--- src/pages/Trip/Trip.jsx
+++ src/pages/Trip/Trip.jsx
@@ -89,7 +89,6 @@ function Trip(props) {
       <h1>{trip.name}</h1>
       <h2>{trip.notes}</h2>
       <h3>{dateFormat(trip.date, "mediumDate", true)}</h3>
-      <Link to={'/checklists'}>Checklists</Link>
       {/* <button type="button">Edit Trip</button> */}
       {trip.name &&
       <button 
