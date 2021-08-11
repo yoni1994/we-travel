f4e6e0a372c43bf2e76d3158adc4f30d474103d2
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-08-11 05:34:58 -0400

    Implement toggle feature

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,40 +10,46 @@ const NavBar = ({ user, handleLogout }) => {
 
 	return (
 		<nav>
-			<div>
-			{user ? (
-				<ul>
-					<li>Welcome, {user.name}</li>
-					<li>
-						<NavLink to='' onClick={handleLogout}>Log out</NavLink>
-					</li>
-					<li>
-						<NavLink to="/users">Users</NavLink>
-					</li>
-					<li>
-						<NavLink to="/home">Home</NavLink>
-					</li>
-					<li>
-						<NavLink to="/trips">Trips</NavLink>
-					</li>
-					<li>
-						<NavLink to="/inspiration">Inspiration</NavLink>
-					</li>
-				</ul>
-			) : (
-				<ul>
-					<li>
-						<NavLink to="/login">Log In</NavLink>
-					</li>
-					<li>
-						<NavLink to="/users">Users</NavLink>
-					</li>
-					<li>
-						<NavLink to="/signup">Sign Up</NavLink>
-					</li>
-				</ul>
-			)}
-			</div>
+			<i 
+			  onClick={handleToggle}
+			  class="fas fa-bars">
+		    </i>
+			{openOrClosed &&
+				<div>
+				{user ? (
+					<ul>
+						<li>Welcome, {user.name}</li>
+						<li>
+							<NavLink to='' onClick={handleLogout}>Log out</NavLink>
+						</li>
+						<li>
+							<NavLink to="/users">Users</NavLink>
+						</li>
+						<li>
+							<NavLink to="/home">Home</NavLink>
+						</li>
+						<li>
+							<NavLink to="/trips">Trips</NavLink>
+						</li>
+						<li>
+							<NavLink to="/inspiration">Inspiration</NavLink>
+						</li>
+					</ul>
+				) : (
+					<ul>
+						<li>
+							<NavLink to="/login">Log In</NavLink>
+						</li>
+						<li>
+							<NavLink to="/users">Users</NavLink>
+						</li>
+						<li>
+							<NavLink to="/signup">Sign Up</NavLink>
+						</li>
+					</ul>
+				)}
+				</div>
+            }
 		</nav>
 	)
 }
