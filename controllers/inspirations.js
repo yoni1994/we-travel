d8ccd017d7b043cdc7805f838c909fbff2c8de43
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-08-11 19:53:59 -0400

    Import dependencies, set up constants and access, and stub up unsplash usage

--- controllers/inspirations.js
+++ controllers/inspirations.js
@@ -1,7 +1,21 @@
-export {
-    index
+import { createApi } from 'unsplash-js'
+import fetch from 'node-fetch'
+URL = require('url').URL;
+
+const BASE_URL = 'https://api.unsplash.com/'
+const RANDOM = 'photos/random'
+
+const UNSPLASH_ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY
+
+const serverApi = createApi({
+    accessKey: UNSPLASH_ACCESS_KEY
+})
+
+
+async function index(req, res){
+    
 }
 
-function index(req, res){
-
+export {
+    index
 }
