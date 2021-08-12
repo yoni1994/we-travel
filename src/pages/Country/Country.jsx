6ad840d34052020a99f6dea68c2c4c198540c943
yoni1994 <yoniklein1994@gmail.com>
2021-08-12 16:59:18 -0500

    stub up countries show page

---
+++ src/pages/Country/Country.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Country.module.css'
+
+
+function Activity(props) {
+ 
+  return (
+    <div className={styles.container}>
+        <h1>test</h1>
+    </div>
+    
+  )
+}
+
+export default Activity
