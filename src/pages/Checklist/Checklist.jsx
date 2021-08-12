5b8b37441a207c802d7934974d206995d1348aca
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-08-12 12:33:14 -0400

    Flesh out Checklist component and add basic styling

--- src/pages/Checklist/Checklist.jsx
+++ src/pages/Checklist/Checklist.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom'
+
+// Styles
+import styles from './Checklist.module.css'
+
+// Services
+import * as checklistService from '../../services/checklistService'
 
 function Checklist(){
+    const { id } = useParams()
+    const [checklist, setChecklist] = useState({})
+
+    useEffect(() => {
+        const fetchChecklist = async () => {
+            try {
+                const checklist = await checklistService.getChecklistById(id)
+                setChecklist(checklist)
+            } catch (error) {
+                throw error
+            }
+        }
+        fetchChecklist()
+        console.log(checklist)
+        return () => { setChecklist(null) }
+      }, [id])
+
+
     return (
-        <div>
-            Checklist
+        <div className={styles.container}>
+            <h1>{checklist.name} Checklist</h1>
         </div>
     )
 }
