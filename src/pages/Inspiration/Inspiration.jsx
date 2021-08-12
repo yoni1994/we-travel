0429acabd4a5106115e047d0eb2ca4686398f02e
jonathanherman1 <jonathan.n.herman@gmail.com>
2021-08-12 00:43:05 -0400

    Import and use inspiration service in Inspiration component to get background image from Unsplash

--- src/pages/Inspiration/Inspiration.jsx
+++ src/pages/Inspiration/Inspiration.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Styles
 import styles from './Inspiration.module.css'
@@ -7,8 +7,18 @@ import styles from './Inspiration.module.css'
 import ImageCard from '../../components/ImageCard/ImageCard';
 
 // Services
+import * as inspirationService from '../../services/inspirationService'
 
 function Inspiration(){
+    const [backgroundImage, setBackgroundImage] = useState('')
+
+    useEffect(() => {
+       inspirationService.index()
+        .then(unsplashImage => setBackgroundImage(unsplashImage))
+    }, []);
+
+    console.log("backgroundImage", backgroundImage);
+
     return (
         <div className={styles.container}>
 
