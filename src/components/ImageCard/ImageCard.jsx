d4f8d73b19c71cb4d37dfd616b8ac35dc85a609d
yoni1994 <yoniklein1994@gmail.com>
2021-08-12 16:06:01 -0500

    add link to inspiration card

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
 
 import styles from './ImageCard.module.css'
 
@@ -9,21 +11,23 @@ import countryPics from '../../assets/data/countryPics';
 
 function ImageCard(){
     return (
-        <div className={styles.container}>
-            {countryPics.map((countryPic, i) => (
-                <div className={styles.card}>
-                        <div className={styles.imageBanner}>
-                            <img src={countryPic.pics.structures} alt="" className={styles.imageBanner}/>
-                        </div>
-                        <div className={styles.cardHeader}>
-                            <h3>{countryPic.country}</h3>
-                        </div>
-                        <div className={styles.cardBody}>
-                            <button>Travel Here</button>
-                        </div>
-                </div>
-            ))}
-        </div>
+        <Link to={`/inspirations/${countryPics.alpha3code}`}>
+            <div className={styles.container}>
+                {countryPics.map((countryPic, i) => (
+                    <div className={styles.card}>
+                            <div className={styles.imageBanner}>
+                                <img src={countryPic.pics.structures} alt="" className={styles.imageBanner}/>
+                            </div>
+                            <div className={styles.cardHeader}>
+                                <h3>{countryPic.country}</h3>
+                            </div>
+                            <div className={styles.cardBody}>
+                                <button>Travel Here</button>
+                            </div>
+                    </div>
+                ))}
+            </div>
+        </Link>
     )
 }
 
