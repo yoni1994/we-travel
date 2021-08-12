333eb48ab950c1350acc2a4a0b4d3e76dde8de56
yoni1994 <yoniklein1994@gmail.com>
2021-08-12 17:00:36 -0500

    stub up countries show page 2

--- src/pages/Country/Country.jsx
+++ src/pages/Country/Country.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import styles from './Country.module.css'
 
 
-function Activity(props) {
+function Country() {
  
   return (
     <div className={styles.container}>
         <h1>test</h1>
     </div>
-    
   )
 }
 
-export default Activity
+export default Country
