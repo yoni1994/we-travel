353df1ff38c2ec45c84da1fd9c8880bd1a39b85b
yoni1994 <yoniklein1994@gmail.com>
2021-08-12 11:45:09 -0500

    change array name from blacklistedCountries to nonDestinations

--- src/assets/data/blacklist.js
+++ src/assets/data/blacklist.js
@@ -1,3 +1,3 @@
-let blacklistedCountries = ['ATA', 'BVT', 'UMI', 'CXR', 'IOT', 'ALA', 'CCK', 'ATF', 'GLP', 'GGY', 'HMD', 'HKG', 'IMN', 'JEY', 'MAC', 'MYT', 'NCL', 'NIU', 'NFK', 'MNP', 'PSE', 'PCN', 'BLM', 'SPM', 'SGS', 'SJM', 'TKL', 'WLF', 'ESH']
+let nonDestinations = ['ATA', 'BVT', 'UMI', 'CXR', 'IOT', 'ALA', 'CCK', 'ATF', 'GLP', 'GGY', 'HMD', 'HKG', 'IMN', 'JEY', 'MAC', 'MYT', 'NCL', 'NIU', 'NFK', 'MNP', 'PSE', 'PCN', 'BLM', 'SPM', 'SGS', 'SJM', 'TKL', 'WLF', 'ESH']
 
 let dangerousCountries = []
